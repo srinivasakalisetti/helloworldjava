7f32b84a57d28b4fb861d29c56e11f4d36037bd9
srinivasakalisetti <82461737+srinivasakalisetti@users.noreply.github.com>
2021-10-02 12:26:55 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,7 @@
 public class helloworldjava {
    public static void main(String[] args) {
     System.out.println("Hello World from Jenkins!!");
+    System.out.println("Activated auto build with polling");  
     }
 }
 
